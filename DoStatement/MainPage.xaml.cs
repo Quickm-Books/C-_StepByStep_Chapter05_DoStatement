718a32f30fce8def66907681e64a64111f90c8a3
quickm <quickm.career@gmail.com>
2023-03-13 21:14:10 -0700

    Complete Do loop exercise

--- DoStatement/MainPage.xaml.cs
+++ DoStatement/MainPage.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -29,7 +18,21 @@ namespace DoStatement
 
         private void showStepsClick(object sender, RoutedEventArgs e)
         {
+            int amount = int.Parse(number.Text);
+            steps.Text = string.Empty;
+            string current = string.Empty;
 
+            do
+            {
+                int nextDigit = amount % 8;
+                amount /= 8;
+
+                int digitCode = '0' + nextDigit;
+                char digit = Convert.ToChar(digitCode);
+                current = digit + current;
+                steps.Text += current + "\n";
+            }
+            while (amount != 0);
         }
     }
 }
